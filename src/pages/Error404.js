ea370d820f034057b0dc3bfcfd987f34c4e3ae06
ekorahy <ekorahy@gmail.com>
2022-10-14 01:44:34 +0700

    update feature in error 404 page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -2,10 +2,14 @@ import React from "react";
 import UrlImage from "../components/main/UrlImage";
 import Error404 from "../assets/images/error404.png";
 import ButtonLink from "../components/main/ButtonLink";
+import LocaleContext from "../contexts/LocaleContext";
 
 function PageNotFound() {
+    const { language } = React.useContext(LocaleContext);
     return (
-        <div className="container">
+        <div className="container text-center text-danger" style={{marginTop: '50px'}}>
+            <h1 className="fw-bold">{language === 'id' ? 'Kesalahan 404' : 'ERROR 404'}</h1>
+            <p>- {language === 'id' ? 'Halaman Tidak Ditemukan' : 'Page Not Found'} -</p>
             <UrlImage
              urlImg={Error404} 
              alt="Home Ilustration Image" 
@@ -15,8 +19,8 @@ function PageNotFound() {
             <div className="d-flex justify-content-center mt-3 mb-5">
                 <ButtonLink
                  link="/" 
-                 label="Back to Home" 
-                 className='btn btn-link fw-bold'
+                 label={language === 'id' ? 'Kembali ke beranda' : 'Back to Home'} 
+                 className='btn btn-link text-info'
                  title="Add New Note" />
             </div>
         </div>
